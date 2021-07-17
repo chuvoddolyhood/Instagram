1637cd3e6258ca7be80c8fc5ac492ac8bc95eb18
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2021-07-17 09:36:52 +0700

    created fragment from navigation bar

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.instagram.Fragment.HomeFragment;
+import com.example.instagram.Fragment.NotificationFragment;
+import com.example.instagram.Fragment.ProfileFragment;
+import com.example.instagram.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        anhXa();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        //Open Fragment Home
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new HomeFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_home: {
+                    selectedFragment = new HomeFragment();
+                    break;
+                }
+                case R.id.nav_search: {
+                    selectedFragment = new SearchFragment();
+                    break;
+                }
+                case R.id.nav_add: {
+                    selectedFragment = null;
+                    startActivity(new Intent(MainActivity.this, AddActivity.class));
+                    break;
+                }
+                case R.id.nav_heart: {
+                    selectedFragment = new NotificationFragment();
+                    break;
+                }
+                case R.id.nav_profile: {
+                    SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                    editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    editor.apply();
+
+                    selectedFragment = new ProfileFragment();
+                    break;
+                }
+            }
+
+            if(selectedFragment != null){
+                //Replacing a fragment with another fragment inside activity group
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, selectedFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+
+            return true;
+        }
+    };
+
+    private void anhXa() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 }
